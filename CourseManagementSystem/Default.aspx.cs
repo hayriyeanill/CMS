34ecd4b28646dec2c1794ba3c91b34be022e5692
Damla Koksal <damlakoksal@outlook.com>
2019-04-23 00:02:27 +0300

    Database error fixed.

--- CourseManagementSystem/Default.aspx.cs
+++ CourseManagementSystem/Default.aspx.cs
@@ -11,37 +11,29 @@ public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        if (IsPostBack)
+        try
         {
-            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["RegiConnectionString"].ConnectionString);
-            conn.Open();
-            string checkuser = "select count(*) from RegisterDataBase where StudentName='" + txtUserID.Text + "'";
-            SqlCommand cmd = new SqlCommand(checkuser, conn);
-            int temp = Convert.ToInt32(cmd.ExecuteScalar().ToString());
+            string connectionString;
+            SqlConnection cnn;
 
-            if (temp == 1)
-            {
-                Response.Write("Student Already Exist");
-            }
+            connectionString = @"Data Source=(localdb)\CMS; Initial Catalog=CMS"; // Database'i kendi bilgilerine göre değiştir.
+            cnn = new SqlConnection(connectionString);
 
-            conn.Close();
+            cnn.Open();
+            Response.Write("asdasdasdads");
+            cnn.Close();
         }
+        catch (Exception ex)
+        {
+            Response.Write("error" + ex.ToString());
+        }
+
     }
 
     protected void btnLogin_Click(object sender, EventArgs e)
     {
         try
         {
-
-            string connectionString;
-            SqlConnection cnn;
-            connectionString = @"Data Source=DESKTOP-281SDJK\LOCALDB#794B4843;Initial Catalog=CMS"; // Database'i kendi bilgilerine göre değiştir.
-            cnn = new SqlConnection(connectionString);
-            cnn.Open();
-
-            Console.WriteLine("oldu");
-            cnn.Close();
-
 
             /* string userId = txtUserID.Text;
              string password = txtPassword.Text;
