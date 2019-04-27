abaf291c421abf7890f76e85f201aae921253f9e
Damla Koksal <damlakoksal@outlook.com>
2019-04-27 18:28:28 +0300

    Fixed login problem.

--- CourseManagementSystem/LoginPage.aspx.cs
+++ CourseManagementSystem/LoginPage.aspx.cs
@@ -7,47 +7,57 @@ using System.Web.UI.WebControls;
 using System.Data.SqlClient;
 using System.Configuration;
 using System.Data;
+using System.Text.RegularExpressions;
 
 public partial class _Default : System.Web.UI.Page
 {
+
     protected void Page_Load(object sender, EventArgs e)
     {
         lblErrorMessage.Visible = false;
     }
 
-
     protected void btnLogin_Click1(object sender, EventArgs e)
     {
-        //sayi disi veri kontrolu lazim
-        using (SqlConnection sqlConn = new SqlConnection(@"Data Source=(localdb)\CMS; Initial Catalog=CMS; integrated Security=True;"))
+        Regex r = new Regex(@"^[0-9]*$"); // userID must be numerical characters.
+        if (!r.IsMatch(txtUserID.Text))
         {
-            try
+            lblErrorMessage.Visible = true;
+            txtUserID.Text = "";
+        }
+        else
+        {
+            using (SqlConnection sqlConn = new SqlConnection(@"Data Source=(localdb)\CMS; Initial Catalog=CMS; integrated Security=True;"))
             {
-                sqlConn.Open();
-                string query = "SELECT COUNT(1) FROM USERS WHERE user_id=@userid AND password=@password";
-                SqlCommand sqlCom = new SqlCommand(query, sqlConn);
-
-                sqlCom.Parameters.AddWithValue("@userid", txtUserID.Text.Trim());
-                sqlCom.Parameters.AddWithValue("@password", txtPassword.Text.Trim());
-
-                int count = Convert.ToInt32(sqlCom.ExecuteScalar()); // 1 gelmesi gerekiyor diger turlu kullanici yok demektir.
-                if (count == 1)
+                try
                 {
-                    Session["userid"] = txtUserID.Text.Trim(); // kullaniciyi bilmek icin
-                    Response.Redirect("HomePage.aspx?userID=" + Session["userid"]);
+                    sqlConn.Open();
+                    string query = "SELECT COUNT(1) FROM USERS WHERE user_id=@userid AND password=@password";
+                    SqlCommand sqlCom = new SqlCommand(query, sqlConn);
+
+                    sqlCom.Parameters.AddWithValue("@userid", txtUserID.Text.Trim());
+                    sqlCom.Parameters.AddWithValue("@password", txtPassword.Text.Trim());
+
+                    int count = Convert.ToInt32(sqlCom.ExecuteScalar()); // Count has to be 1 otherwise, user does not exist.
+                    if (count == 1)
+                    {
+                        Session["userid"] = txtUserID.Text.Trim(); // to know which user signed in.
+                        Response.Redirect("HomePage.aspx?userID=" + Session["userid"]);
+                    }
+                    else
+                    {
+                        lblErrorMessage.Visible = true;
+                        txtUserID.Text = "";
+                    }
+                    sqlConn.Close();
                 }
-                else
+                catch (Exception ex)
                 {
-                    lblErrorMessage.Visible = true;
+                    Response.Write(ex.ToString());
                 }
-                sqlConn.Close();
-            }
-            catch (Exception ex)
-            {
-                Response.Write("error" + ex.ToString());
             }
         }
     }
 }
 
-// logout butonunda session.abandon kullanmayi unutma
+// logout butonunda session.abandon kullanmayi unutma   
