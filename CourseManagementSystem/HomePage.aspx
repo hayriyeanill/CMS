5d0acd91377b3f06dcec2b002055a422e67ec5c0
hayriyeanill <37445083+hayriyeanill@users.noreply.github.com>
2019-04-29 20:40:23 +0300

    Master Page problem fixed and added assignments in home page.

--- CourseManagementSystem/HomePage.aspx
+++ CourseManagementSystem/HomePage.aspx
@@ -3,9 +3,12 @@
 <%-- Add content controls here --%>
 <asp:Content ID="Content1" runat="server" contentplaceholderid="ContentPlaceHolder1">
    
-    <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+    
+    <asp:BulletedList ID="BulletedList2" runat="server" style="margin-bottom: 0px">
+    </asp:BulletedList>
     <asp:BulletedList ID="BulletedList1" runat="server">
     </asp:BulletedList>
-   
+   <asp:Label ID="Label1" runat="server" Visible="False"></asp:Label>
+    <asp:Label ID="Label2" runat="server" Visible="False"></asp:Label>
 </asp:Content>
 
--- CourseManagementSystem/HomePage.aspx.cs
+++ CourseManagementSystem/HomePage.aspx.cs
@@ -24,8 +24,24 @@ public partial class HomePage :  System.Web.UI.Page
             BulletedList1.Items.Add(Label1.Text);
         }
 
-        BulletedList1.Items.Add(Label1.Text);
+        
         dr.Close();
         sqlConn.Close();
+
+        SqlConnection sqlConn2 = new SqlConnection(@"Data Source=(localdb)\CMS; Initial Catalog=CMS; integrated Security=True;");
+        sqlConn2.Open();
+        string query2 = "SELECT COURSES.COURSE_INSTRUCTOR,ASSIGNMENTS.COURSE_ID,ASSIGNMENTS.ASSIGNMENT_NO FROM COURSES, ASSIGNMENTS where COURSES.COURSE_ID = ASSIGNMENTS.COURSE_ID";
+        SqlCommand sqlCom2 = new SqlCommand(query2, sqlConn2);
+        SqlDataReader dr2;
+        dr2 = sqlCom2.ExecuteReader();
+        while (dr2.Read())
+        {
+            Label2.Text = dr2["COURSE_INSTRUCTOR"].ToString() + " added Assignment " + dr2["ASSIGNMENT_NO"].ToString() + " in " + dr2["COURSE_ID"].ToString();
+            BulletedList2.Items.Add(Label2.Text);
+        }
+       
+        dr2.Close();
+        sqlConn2.Close();
     }
+
 }
