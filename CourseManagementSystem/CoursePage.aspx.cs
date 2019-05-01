b28f04d6db5191eaf9e381ebd429940bbf1675a9
Damla Koksal <damlakoksal@outlook.com>
2019-05-01 16:47:00 +0300

    .

--- CourseManagementSystem/CoursePage.aspx.cs
+++ CourseManagementSystem/CoursePage.aspx.cs
@@ -10,13 +10,16 @@ public partial class CoursePage : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+       // string courses = Request.QueryString["courses"];
+        //Session["courses"] = courses;
+
         SqlConnection sqlConn = new SqlConnection(@"Data Source=(localdb)\CMS; Initial Catalog=CMS; integrated Security=True;");
         sqlConn.Open();
-        //string query = "SELECT COURSES.COURSE_ID, COURSES.COURSE_INSTRUCTOR, ASSIGNMENTS.ASSIGNMENT_NO FROM COURSES, ASSIGNMENTS WHERE COURSES.COURSE_ID = ASSIGNMENTS.COURSE_ID AND COURSES.COURSE_ID='SEN3304' ";
+        string query = "SELECT COURSES.COURSE_ID, COURSES.COURSE_INSTRUCTOR, ASSIGNMENTS.ASSIGNMENT_NO FROM COURSES, ASSIGNMENTS WHERE COURSES.COURSE_ID = ASSIGNMENTS.COURSE_ID AND COURSES.COURSE_ID='SEN3304' ";
         //string query = "SELECT COURSES.COURSE_ID, COURSES.COURSE_INSTRUCTOR, ASSIGNMENTS.ASSIGNMENT_NO FROM COURSES, ASSIGNMENTS WHERE COURSES.COURSE_ID = ASSIGNMENTS.COURSE_ID AND COURSES.COURSE_ID = 'SEN3006'";
         //string query = "SELECT COURSES.COURSE_ID, COURSES.COURSE_INSTRUCTOR, ASSIGNMENTS.ASSIGNMENT_NO FROM COURSES, ASSIGNMENTS WHERE COURSES.COURSE_ID = ASSIGNMENTS.COURSE_ID AND COURSES.COURSE_ID = 'SEN2212'";
         //string query = "SELECT TOP (1000) [USER_ID],[COURSE_ID], [RESOURCE] FROM[CMS].[dbo].[STUDENTS], [CMS].[dbo].[RESOURCES] WHERE USER_ID ='" + 
-        string query = "SELECT STUDENTS.COURSE_ID, COURSES.COURSE_ID, COURSES.COURSE_INSTRUCTOR, RESOURCES.RESOURCE FROM STUDENTS, COURSES, RESOURCES WHERE STUDENTS.COURSE_ID=COURSES.COURSE_ID and COURSES.COURSE_ID = RESOURCES.COURSE_ID and STUDENTS.USER_ID = '" + Session["userid"] +"'";
+       // string query = "SELECT STUDENTS.COURSE_ID, COURSES.COURSE_ID, COURSES.COURSE_INSTRUCTOR, RESOURCES.RESOURCE FROM STUDENTS, COURSES, RESOURCES WHERE STUDENTS.COURSE_ID=COURSES.COURSE_ID and COURSES.COURSE_ID = RESOURCES.COURSE_ID and STUDENTS.USER_ID = '" + Session["userid"] + "'";
         //string query2 = "SELECT STUDENTS.USER_ID, STUDENTS.COURSE_ID, COURSES.COURSE_INSTRUCTOR, RESOURCES.RESOURCE FROM STUDENTS, COURSES, RESOURCES WHERE STUDENTS.USER_ID ='" + Session["userid"] + "' and STUDENTS.COURSE_ID = RESOURCES.COURSE_ID";
         //string query = "SELECT COURSE_ID, COURSE_INSTRUCTOR, RESOURCE from RESOURCES"; select STUDENTS.COURSE_ID,COURSES.COURSE_ID, COURSES.COURSE_INSTRUCTOR,RESOURCES.RESOURCE 
 
@@ -31,15 +34,15 @@ public partial class CoursePage : System.Web.UI.Page
 
             // DropDownList1.Items.Insert(0, new ListItem(DropDownList1.DataValueField = dr["COURSE_ID"].ToString(), "0"));
             //courses içinde seçilen derse göre dersin güncellemesi görülecektir.
-           
-           Label1.Text = dr["COURSE_INSTRUCTOR"].ToString() + " added " +dr["RESOURCE"].ToString() + " in " + dr["COURSE_ID"].ToString();
-           
+
+            Label1.Text = dr["COURSE_INSTRUCTOR"].ToString() + " added assignment" + dr["ASSIGNMENT_NO"].ToString() + " in " + dr["COURSE_ID"].ToString();
+
             //Label2.Text = dr["COURSE_INSTRUCTOR"].ToString() + " added grades in " + dr["COURSE_ID"];
             //Label1.Text = dr["COURSE_INSTRUCTOR"].ToString() + " added " + dr["RESOURCE"] + " in " + dr["COURSE_ID"];
             BulletedList2.Items.Add(Label1.Text);
 
         }
-      
+
         sqlConn.Close();
     }
 }
